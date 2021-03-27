728977fc142147145bd7de98552486c88e9b0e22
Rajat Goyal <rajatgoyaloficial007@gmail.com>
2021-03-28 00:02:15 +0530

    stage2

--- form.php
+++ form.php
@@ -3,15 +3,18 @@
     $Email = $_POST['Email'];
     $Subject = $_POST['Subject'];
     $Message = $_POST['Message'];
-?>
 
-$conn = new mysqli('localhost','root','',test);
-if($conn->connect_error){
-    die('Connection Failed  :  '.$conn->connect_error');    
-}else{
-    $stmt = $conn->prepare("insert into registration(Name,Email,Subject,Message) values(?,?,?,?)")
-    $stmt->bind_param("ssss",$Name, $Email, $Subject, $Message);
-    $stmt->execute();
-    echo "registration sucessfull...";
-    $stmt->close()
-}
+	$conn = new mysqli('localhost','root','','unaux_28218872_Contact');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(Name, Email, Subject, Message) values(?, ?, ?, ?)");
+		$stmt->bind_param("ssss",$Name, $Email, $Subject, $Message);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
